5e7bfdfd436ee3b2bdcad9724957cb27309b938c
Igor De spirlet <ide-spir@mi-c2-p1.s19.be>
2022-02-21 17:04:16 +0100

    GNL bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 15:37:40 by ide-spir          #+#    #+#             */
+/*   Updated: 2022/02/21 16:57:16 by ide-spir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_to_backup(int fd, char *backup);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *backup, char *buffer);
+size_t	ft_strlen(const char *s);
+char	*ft_get_line(char *backup);
+char	*ft_backup(char *backup);
+
+#endif
